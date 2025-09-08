a6cbedbb761d63a372d6df1b6a440992940d49ef
Arun cs <aruncs31ss@gmail.com>
2025-09-08 16:45:53 +0530

    added option to create qualification

--- src/apis/api.py
+++ src/apis/api.py
@@ -22,3 +22,32 @@ class STAFF_API:
         else:
             print(f"Failed to fetch staffs. Status Code: {response.status_code}, Response: {response.text}")
             return []
+    def register_qualification(self, staff_id: str, data: dict) :
+        """Register a new qualification for a staff member."""
+        # url = f"{self.base_url}/{staff_id}/qualifications"
+        # print(f"URL for qualification registration: {url}")
+        # method = "POST"
+        # data["staff_id"] = staff_id
+        # print(data)
+        # response = requests.request(method, url, json=data)
+        
+        # if response.status_code == 201:
+        #     print(f"Qualification registered successfully for Staff ID: {staff_id}")
+        #     return response.json()
+            
+        # else:
+        #     print(f"Failed to register qualification for Staff ID: {staff_id}. Status Code: {response.status_code}, Response: {response.text}")
+        #     return None
+        """Register a new qualification for a staff member"""
+        headers = {"Content-Type": "application/json", "Accept": "application/json"}
+        url = f"{self.base_url}/{staff_id}/qualification"
+        print(f"POST: URL {url}")
+        print(f"Data: {data}")
+        response = requests.post(url, json=data, headers=headers)
+        print(f"Status Code: {response.status_code}")
+
+        if response.status_code == 201:
+            return response.json()
+        else:
+            print(f"Error: {response.text}")
+            return None
--- src/app.py
+++ src/app.py
@@ -4,7 +4,8 @@ from apis.api import STAFF_API
 def print_menu() -> int:
     print("1. Register Staff")
     print("2. Get All Staffs")
-    print("3. View Staff Details")
+    # print("3. View Staff Details")
+    print("3. Create Qualification")
     print("4. Exit")
     return int(input())
 
@@ -27,6 +28,22 @@ def get_all_staffs(api: STAFF_API):
     staffs = api.get_all_staffs()
     for staff in staffs:
         print(f"ID: {staff.get('staff_id')}, Name: {staff.get('full_name')}")
+
+def create_new_qualification(api: STAFF_API):
+    staff_id = input("Enter Staff ID: ")
+    print("Enter Qualification Details:")
+    degree  = int(input("Degree (as integer): "))
+    discipline = input("Discipline: ")
+    data = {
+        "degree": degree,
+        "discipline": discipline
+    }
+    resp  = api.register_qualification(staff_id, data = data)
+    print(f"Qualification Response: {resp}")
+
+
+
+
 def main():
     
     choice = print_menu()
@@ -36,6 +53,9 @@ def main():
     elif choice == 2:
         api = STAFF_API("http://127.0.0.1:9090/api/v1/staff")
         get_all_staffs(api)
+    elif choice == 3:
+        api = STAFF_API("http://127.0.0.1:9090/api/v1/staff")
+        create_new_qualification(api)
     elif choice == 4:
         exit()
     else:
