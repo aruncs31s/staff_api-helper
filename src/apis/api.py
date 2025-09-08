35545a9b83cd3c8c5cec9d0d291453dea71fba00
Arun cs <aruncs31ss@gmail.com>
2025-09-08 12:57:05 +0530

    added the option to view registered staffs

--- src/apis/api.py
+++ src/apis/api.py
@@ -1,14 +1,24 @@
 import requests
 URL="http://127.0.0.1:9090/api/v1/staff"
 
-class API:
-    pass
-
-def register_staff(staff_data: dict[str, str | int]) -> None:
-    print(f"Registering staff: {staff_data.get('full_name')} With ID: {staff_data.get('staff_id')} ")
-    method = "POST" 
-    response = requests.request(method, URL, json=staff_data)
-    if response.status_code == 201:
-        print(f"Staff {staff_data.get('full_name')} Registered Successfully")
-    else:
-        print(f"Failed to create staff {staff_data.get('full_name')}. Status Code: {response.status_code}, Response: {response.text}")
+class STAFF_API:
+    def __init__(self,base_url: str):
+        self.base_url = base_url
+    def register_staff(self, staff_data: dict[str, str | int]) -> None:
+        print(f"Registering staff: {staff_data.get('full_name')} With ID: {staff_data.get('staff_id')} ")
+        method = "POST" 
+        response = requests.request(method, self.base_url, json=staff_data)
+        if response.status_code == 201:
+            print(f"Staff {staff_data.get('full_name')} Registered Successfully")
+        else:
+            print(f"Failed to create staff {staff_data.get('full_name')}. Status Code: {response.status_code}, Response: {response.text}")
+    def get_all_staffs(self) -> list[dict[str, str | int]]:
+        """Get all staffs from the Go API Server."""
+        method = "GET"
+        response = requests.request(method, self.base_url)
+        response_data = response.json()
+        if response.status_code == 200:
+            return response_data.get("data", [])
+        else:
+            print(f"Failed to fetch staffs. Status Code: {response.status_code}, Response: {response.text}")
+            return []
--- src/app.py
+++ src/app.py
@@ -1,13 +1,14 @@
 from files.files import FileHandler
 from formater.staff_info import StaffInfoFormatter
-from apis.api import register_staff
+from apis.api import STAFF_API
 def print_menu() -> int:
     print("1. Register Staff")
-    print("2. View Staff Details")
-    print("3. Exit")
+    print("2. Get All Staffs")
+    print("3. View Staff Details")
+    print("4. Exit")
     return int(input())
 
-def register_new_staff():
+def register_new_staff(api: STAFF_API):
     file_handler = FileHandler()
     staff_details = file_handler.get_staff_details()
     # formatter = StaffInfoFormatter(staff_details)
@@ -20,16 +21,23 @@ def register_new_staff():
     if option < 1:
         return
     selected_staff = staff_details[option - 1]
-    register_staff(selected_staff)
+    api.register_staff(selected_staff)
     print_menu()
+def get_all_staffs(api: STAFF_API):
+    staffs = api.get_all_staffs()
+    for staff in staffs:
+        print(f"ID: {staff.get('staff_id')}, Name: {staff.get('full_name')}")
 def main():
+    
     choice = print_menu()
     if choice == 1:
-        register_new_staff()
-    # elif choice == 2:
-    #     view_staff_details()
-    # elif choice == 3:
-    #     exit()
+        api = STAFF_API("http://127.0.0.1:9090/api/v1/staff")
+        register_new_staff(api)
+    elif choice == 2:
+        api = STAFF_API("http://127.0.0.1:9090/api/v1/staff")
+        get_all_staffs(api)
+    elif choice == 4:
+        exit()
     else:
         print("Invalid choice. Please try again.")
 
